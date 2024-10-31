210c3f79a8eea8b5d8e1483d5f5a98d71d3fb090
BeterrabaComTorrada <luffyzim@gmail.com>
2024-10-30 21:12:03 -0300

    justOneMethod

--- src/main/java/lo/zqm/fhg/plm/GenericApi.java
+++ src/main/java/lo/zqm/fhg/plm/GenericApi.java
@@ -20,66 +20,68 @@ public class GenericApi extends urlConstantApi{
 		GET, POST, PATCH, PUT, DELETE;
 	}
 	
-															 				       
+
 	public <T> T genericRequest(String url,
-								RequestType type,// Response of API
-								Class<T> responseType,
-								boolean list,//éLista?
-								Map<String, Object> queryParams,//pAthparams
-								Object... params) {
+			RequestType type,// Response of API
+			Class<T> responseType,
+			boolean list,//éLista?
+			Map<String, Object> queryParams,//pAthparams
+			Object... params) {
 
-		 switch (type) {
-		 
-         case GET:
-             return executeGet(url, responseType, queryParams);
-         case POST:
-             return executePost(url, responseType, params);
-         case PATCH:
-             return executePatch(url, responseType, params);
-         case PUT:
-             return executePut(url, responseType, params);
-         case DELETE:
-             executeDelete(url, queryParams);
-             return null;
-             
+		URI uri = buildUriWithParams(url, queryParams);
+		HttpEntity<Object> requestEntity = (params != null && params.length > 0) ? new HttpEntity<>(params[0]) : null;
+		ResponseEntity<T> response;
+
+		switch (type) {
+		
+		case GET:
+			response = restTemplate.exchange(uri, HttpMethod.GET, null, responseType);
+			
+			break;
+			
+		case POST:
+			response = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, responseType);
+			
+			break;
+			
+		case PUT:
+			response = restTemplate.exchange(uri, HttpMethod.PUT, requestEntity, responseType);
+			
+			break;
+			
+		case PATCH:
+			response = restTemplate.exchange(uri, HttpMethod.PATCH, requestEntity, responseType);
+			
+			break;
+			
+		case DELETE:
+			restTemplate.delete(uri);
+			
+			return null;
+			
 		default:
-			throw new IllegalArgumentException("deu ruim" + type);
+			throw new IllegalArgumentException("Unsupported request type: " + type);
 		}
-
+		
+		return response.getBody();
 	}
-    private <T> T executeGet(String url, Class<T> responseType, Map<String, Object> queryParams) {
+	
+//    private <T> T executeGet(String url, Class<T> responseType, Map<String, Object> queryParams) {
 //    	URI uri = buildUriWithParams(url, queryParams);
-    	final String url1 = String.format(url, queryParams);
-        return restTemplate.getForObject(url1, responseType);
-    }	
-    
+//    	final String url1 = String.format(url, queryParams);
+//        return restTemplate.getForObject(url1, responseType);
+	
 //    Map<String, Object> queryParams = new HashMap<>();
 //    queryParams.put("page", 1);
 //    queryParams.put("size", 10);
     
     
-    private <T> T executePost(String url, Class<T> responseType, Object... params) {
-		HttpEntity<Object> request = new HttpEntity<>(params[0]);
-		ResponseEntity<T> response = restTemplate.postForEntity(url, request, responseType);
-		return response.getBody();
-	}
-
-    private <T> T executePut(String url, Class<T> responseType, Object... params) {
-		HttpEntity<Object> request = new HttpEntity<>(params[0]);
-		ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.PUT, request, responseType);
-		return response.getBody();
-	}
-
-	private <T> T executePatch(String url, Class<T> responseType, Object... params) {
-		HttpEntity<Object> request = new HttpEntity<>(params[0]);
-		ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.PATCH, request, responseType);
-		return response.getBody();
-	}
-
-	private void executeDelete(String url, Map<String, Object> queryParams) {
-        URI uri = buildUriWithParams(url, queryParams);
-        restTemplate.delete(uri);
+    private <T> T executeRequest(String url, HttpMethod method, Class<T> responseType, Object... params) {
+        HttpEntity<Object> request = params.length > 0 ? new HttpEntity<>(params[0]) : null;
+        ResponseEntity<T> response = restTemplate.exchange(url, method, request, responseType);
+        return response.getBody();
     }
+
 	
 	private URI buildUriWithParams(String url, Map<String, Object> queryParams) {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
@@ -94,30 +96,26 @@ public class GenericApi extends urlConstantApi{
 	   public static void main(String[] args) {
 	        GenericApi genericApi = new GenericApi();
 
-	        // GET All (List)
-	        String listUrl2= "URL_ESPECIFICA";
+	        // Get all List
+	        String listUrl2 = "URL_ESPECIFICA";
 	        Map<String, Object> listParams = Map.of("page?", 1, "size?", 10);
-	        System.out.println("GET All: " + genericApi.genericRequest(listUrl2, RequestType.GET, String.class, true, listParams));
-	        
-	        
-	        // GET By ID
+	        System.out.println("GET All: " + genericApi.executeRequest(listUrl2, HttpMethod.GET, String.class, listParams));
+
+	        // Getbid
 	        String getByIdUrl = "URL_ESPECIFICA_PARA_GET_BY_ID";
 	        Map<String, Object> getByIdParams = Map.of("expand", "details");
-	        															//url 		tipo de request   responseClass éLista?  PAthPArams
-	        System.out.println("GET by ID: " + genericApi.genericRequest(getByIdUrl, RequestType.GET, String.class, false, getByIdParams));
+	        System.out.println("GET by ID: " + genericApi.executeRequest(getByIdUrl, HttpMethod.GET, String.class, getByIdParams));
 
-	        
-	        // PUT Update enviando body)
+	        // PUT Update enviando body
 	        String putUrl = "URL_ESPECIFICA_PARA_PUT";
-	        Map<String, Object> putByIdParams = Map.of("expand", "details");
 	        LegumesDto legumesToUpdate = new LegumesDto();
 	        legumesToUpdate.setName("Updated Name");
-	        System.out.println("PUT Update: " + genericApi.genericRequest(putUrl, RequestType.PUT, LegumesResponseDto.class, false, null, legumesToUpdate, null));
+	        System.out.println("PUT Update: " + genericApi.executeRequest(putUrl, HttpMethod.PUT, LegumesResponseDto.class, null, legumesToUpdate));
 
 	        // DELETE
 	        String deleteUrl = "URL_ESPECIFICA_PARA_DELETE";
 	        Map<String, Object> deleteParams = Map.of("confirm", "true");
-	        genericApi.genericRequest(deleteUrl, RequestType.DELETE, null, false, deleteParams);
+	        genericApi.executeRequest(deleteUrl, HttpMethod.DELETE, Void.class, deleteParams);
 	        System.out.println("DELETE completed");
 	    }
 }
